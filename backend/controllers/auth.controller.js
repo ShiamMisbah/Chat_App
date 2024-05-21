f080ef9c30b2215a41a63ecbfc6ab38619de4233
Shiam Misbah <shiam.misbah@sebpo.com>
2024-05-22 03:40:31 +0600

    Backend Completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,9 @@
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
     try {
-        console.log(req.body);
         const {fullName, userName, password, confirmPassword, gender } = req.body;
 
         if (password !== confirmPassword){
@@ -14,7 +15,9 @@ export const signup = async (req, res) => {
             return res.status(400).json({error: "Username Exists"});
         }
 
-        // Hass Pass here
+        // Hash Pass here
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
@@ -22,21 +25,30 @@ export const signup = async (req, res) => {
         const newUser = new User({
             fullName,
             userName,
-            password,
+            password: hashedPassword,
             gender,
             profilePic: gender === "male" ? boyProfilePic : girlProfilePic
         })
 
-        await newUser.save();
+        if (newUser){
+            // Generate JWT Token here
+            generateTokenAndSetCookie(newUser._id, res);
 
-        res.status(201).json({
-            _id: newUser._id,
-            fullName: newUser.fullName,
-            userName: newUser.userName,
-            password: newUser.password,
-            gender: newUser.gender,
-            profilePic: newUser.profilePic
-        })
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                userName: newUser.userName,
+                password: newUser.password,
+                gender: newUser.gender,
+                profilePic: newUser.profilePic
+            })
+        }
+        else{
+            return res.status(400).json({error: "Invalid User Data"});
+        }
+
 
     } catch (error) {
         console.log("Error in signup Controller", error.message);
@@ -44,10 +56,37 @@ export const signup = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
-    console.log("login User");
+export const login = async (req, res) => {
+    try {
+        const {userName, password} = req.body;
+        const user = await User.findOne({userName});
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+
+        if (!user || !isPasswordCorrect){
+            return res.status(400).json({error: "Invalid Username or Password"});
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            userName: user.userName,
+            profilePic: user.profilePic
+        })
+
+    } catch (error) {
+        console.log("Error in login Controller", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
 }
 
 export const logout = (req, res) => {
-    console.log("logout User");
+    try {
+        res.cookie("jwt", "", { maxAge: 0 })
+        res.status(200).json({message: "Logged Out succesfully."})
+    } catch (error) {
+        console.log("Error in logout Controller", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
-import authRoutes from './routes/auth.routes.js'
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
+
 import connectToMongoDB from './db/connectToMongoDb.js';
 
 const app = express();
@@ -11,9 +15,12 @@ dotenv.config();
 
 
 // MiddleWare
-app.use('/api/auth', authRoutes)
-
 app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res)=> {
     res.send("API is running.")
