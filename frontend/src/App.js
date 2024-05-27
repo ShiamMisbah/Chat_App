e7227a65e44e21e2a3be35a98bb62f31ee4cc960
Shiam Misbah <shiam.misbah@sebpo.com>
2024-05-27 15:43:43 +0600

    Ui design completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,3 @@
-import Login from "./pages/login/Login";
-import Signup from "./pages/signup/Signup"
 import Home from "./pages/home/Home"
 
 function App() {
--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Messages from './Messages';
 import MessageInput from './MessageInput';
+import { TiMessages } from "react-icons/ti";
 
 const MessageContainer = () => {
-    const noChatSelected = false;
+    const noChatSelected = true;
   return (
     <div className=' md:min-w-[450px] flex flex-col '>
         {noChatSelected ? (<NoChatSelected />) : (
@@ -25,6 +26,8 @@ const NoChatSelected = () => {
         <div className="flex items-center justify-center w-full h-full">
             <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
                 <p>Welcome ✋ Shiam Misbah 🔥</p>
+                <p>Select a chat to start messaging.</p>
+                <TiMessages className='text-3xl md:text-6xl text-center' />
             </div>
         </div>
     )
